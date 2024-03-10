f71ffa07c2af2d999b5fed40e8caade260da890d
DWTxgamer <100069709+DWTxgamer@users.noreply.github.com>
2024-03-10 11:09:22 +0300

    Add files via upload

---
+++ LLM elite website/LLM Consulting Agency v2/logic.js
@@ -0,0 +1,26 @@
+{/* <div class="nav-bar" id="navbar">
+<div class="top-section">
+<a href="#">
+<img src="./phoneUse.PNG" alt="" class="PhoneUseNav">
+</a>
+
+<a href="#">
+<img src="./emailUse.PNG" alt="" class="EmailUseNav">
+</a>
+<div class="SearchButton">
+<i class="fa-solid fa-magnifying-glass" id="search-button" onclick="search()"></i>
+</div>
+</div>
+
+<div class="main-section" id="main-part">
+<img src="./WebsiteLogo.PNG" alt="">
+
+<div class="MainNavLinksHolder">
+<a href="#" id="Active_link" class="Nav-links">Immigration</a>
+<a href="#" class="Nav-links">Employment</a>
+<a href="#" class="Nav-links">Human Resources</a>
+<a href="#" class="Nav-links">Global Mobility</a>
+
+</div>
+</div>
+ */}
